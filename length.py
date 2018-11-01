5b111ed97317e0bd8db99dba60c238d95ce1bf0d
Salvador <salvador.tj@gmail.com>
2018-10-31 19:27:33 -0600

    Auto-saved

--- length.py
+++ length.py
@@ -1,14 +1,14 @@
 phrase = "How long are the words in this phrase"
-# words = phrase.split()
+words = phrase.split()
 
-# #print(words)
+#print(words)
 
-# def word_lenghts(w):
-#     return len(w)
+def word_lenghts(w):
+    return len(w)
 
-# print(list(map(word_lenghts,words)))
+print(list(map(word_lenghts,words)))
 
-print(list(map(lambda w: len(w),phrase.split())))
+# print(list(map(lambda w: len(w),phrase.split())))
 
 # def word_lengths(phrase):
 #   return list(map(len, phrase.split()))
